835ac783e5e7581b08d81b3579c90fe9ef695d1e
MartinQuinteroR <martinquinrin@gmail.com>
2026-08-10 20:38:09 -0500

    Segundo comentario para Sync Changes

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ app.config.suppress_callback_exceptions = True
 
 # Load data from csv
 # Agregando comentario para commit en nueva rama
+# Agregando segundo comentario para Sync Changes
 def load_data():
     # To do: Completar la función
         # To do: Completar la función
